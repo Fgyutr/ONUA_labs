5dc17c8623aa14768b9970dc8fdec7810d2a650e
Fgyutr <55824331+Fgyutr@users.noreply.github.com>
2022-11-04 22:05:56 +0200

    1

---
+++ Lab struct/Lab11/Lab11/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab11
+//
+//  Created by Максим Мозговой on 04.11.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ Lab struct/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string> 
+using namespace std;
+
+void SincAutomat()
+{
+    string s;
+    cout << "Введите строку: "; 
+    cin >> s;
+    pair<char, bool> point;
+    point = make_pair<char, bool>("A", true);
+}
